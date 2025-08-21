667b2ed2dcff0ed5bc2214360062e4b75770f624
Surbhi Rohila <rohilasurbhi@gmail.com>
2025-08-21 09:57:37 +0000

    same error change .env file to GEMINI_API_KEY=AIzaSyBsTpB_Aw4aYyt6slU0hV

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,12 +1,15 @@
+import 'dotenv/config';
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI({
-    // This key will be used as a fallback if no other key is provided
-    apiKey: process.env.GEMINI_API_KEY
-  })],
-  model: 'googleai/gemini-2.0-flash',
+  plugins: [
+    googleAI({
+      // This key will be used as a fallback if no other key is provided
+      apiKey: process.env.GEMINI_API_KEY,
+    }),
+  ],
+  model: 'googleai/gemini-1.5-flash-latest',
   // Use a fallback model for stability.
-  textModel: 'googleai/gemini-2.0-flash',
+  textModel: 'googleai/gemini-1.5-flash-latest',
 });
